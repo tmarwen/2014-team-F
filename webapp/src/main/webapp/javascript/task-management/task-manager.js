b8d1ab798b231e4cf8d7c86148b7ef88319d158e
tmarwen <marwen.trabelsi.insat@gmail.com>
2014-06-28 21:19:51 +0100

    Implement add task form invokation services

--- webapp/src/main/webapp/javascript/task-management/task-manager.js
+++ webapp/src/main/webapp/javascript/task-management/task-manager.js
@@ -8,7 +8,7 @@
 
   var portal = window.eXo.env.portal;
 
-  var taskManager = {
+  var TaskManagerUtil = {
     rest: (portal.rest) ? portal.rest : 'rest',
     portalName: (portal.portalName) ? portal.portalName : 'classic',
     context: (portal.context) ? portal.context : '/portal',
@@ -17,17 +17,90 @@
     restTaskManagerURI:  'task-rest-manager'
   };
 
-  var restTaskManagerURL = "/" + taskManager.rest + "/" + taskManager.restTaskManagerURI;
+  var restTaskManagerURL = "/" + TaskManagerUtil.rest + "/" + TaskManagerUtil.restTaskManagerURI;
+  var userSpaceRESTUrl = "/" + TaskManagerUtil.rest + TaskManagerUtil.context + "/social/spaces/mySpaces/show.json";
+  var userSpaces = [];
 
-  $.getJSON(restTaskManagerURL + "/" + "getAllTask", function(data){
-    var items = [];
-    $.each( data, function( key, val ) {
-      items.push( "<li id='" + key + "'>" + val + "</li>" );
-      console.log(val);
+  $("#taskOperationTabs").tabs();
+
+  // initialize input task dates
+  $('#startEndTaskDateCouple .time').timepicker({
+    'showDuration': true,
+    'timeFormat': 'g:ia'
+  });
+
+  $('#startEndTaskDateCouple .date').datepicker({
+    'format': 'yyyy-m-d',
+    'autoclose': true
+  });
+
+  //Setup task type fields
+  $("#taskTypeWrapper").on('change', 'input:radio', function() {
+    var value = this.value;
+    if (value == "Personal") {
+      $("#taskProject").prop("disabled", true);
+      $("#taskAssignee").prop("disabled", true);
+    }
+    else {
+      $("#taskProject").prop("disabled", false);
+      $("#taskAssignee").prop("disabled", false);
+    }
+  });
+
+  $.getJSON(userSpaceRESTUrl, function(data){
+    var mySpaces = data.spaces;
+    var $taskProjectSelectList = $('select#taskProject');
+    clearTaskProjectList();
+    $.each(mySpaces, function( key, val ) {
+      var spaceName = val.name;
+      userSpaces.push(val);
+      $("<option></option>", {value: spaceName, text: val.displayName})
+        .data({
+          spaceName: spaceName,
+          spaceUrl: val.spaceUrl,
+          groupId: val.groupId,
+          url: val.url
+        })
+        .appendTo($taskProjectSelectList);
     });
   });
 
-  $("#tabs").tabs();
+  $("#addTaskForm").on('change', 'select#taskProject', function() {
+    clearTaskAssigneeList();
+    var $selectedSpace = $(this).find(':selected');
+    var spaceUrl = $selectedSpace.data('spaceUrl');
+    var $taskAssigneeSelectList = $('select#taskAssignee');
+    $.getJSON(buildSpaceMemberUrl(spaceUrl), function(data){
+      var spaceMembers = data.names;
+      $.each(spaceMembers, function( key, val ) {
+        $("<option></option>", {value: val, text: val})
+          .data({
+            userName: val
+          })
+          .appendTo($taskAssigneeSelectList);
+      });
+    });
+  });
 
+  function clearTaskProjectList() {
+    $("select#taskProject option")
+      .not(".taskProjectQualifier")
+      .remove();
+  }
+
+  function clearTaskAssigneeList() { // TODO
+    $("select#taskAssignee option")
+      .not(".taskAssigneeQualifier")
+      .remove();
+  }
+
+  function buildSpaceMemberUrl(spaceUrl) {
+    return "/" + TaskManagerUtil.rest
+      + "/social/people/suggest.json"
+      + "?nameToSearch="
+      + "&currentUser=" + TaskManagerUtil.userName
+      + "&typeOfRelation=member_of_space"
+      + "&spaceUrl=" + spaceUrl;
+  }
 
 })(jQuery);
